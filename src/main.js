001313a7c6938798e1e18af0d45e764e608d43d1
ridouchire <ridouchire@gmail.com>
2022-01-31 18:26:53 +0400

    [#10] К постам можно прикреплять изображения.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './components/App.vue'
 import router from './router'
-import { Toast, Sidebar, Menu, Button, Modal, Input, Field, Tag, Switch, Collapse, Pagination } from 'buefy'
+import { Toast, Sidebar, Menu, Button, Modal, Input, Field, Tag, Switch, Collapse, Pagination, Upload, Icon } from 'buefy'
 import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = true
@@ -16,6 +16,8 @@ Vue.use(Tag)
 Vue.use(Switch)
 Vue.use(Collapse)
 Vue.use(Pagination)
+Vue.use(Upload)
+Vue.use(Icon)
 
 new Vue({
   router,
